0e3e1fe4655ab0f9b5c2f313e6aa82129bae557f
aderogbasamuel <aderogbasamueladeolu@gmail.com>
2026-07-30 02:12:14 +0100

    please work

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,32 +1,34 @@
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from "next/server";
+
 import { prisma } from "../../../lib/db";
 interface PageProps {
-  params: Promise<{ id: string }>; // Typed as a Promise
+    params: Promise<{ id: string }>; // Typed as a Promise
 }
 export async function GET(
-  req: NextRequest,
-  { params }: PageProps,
+    req: NextRequest,
+    { params }: PageProps,
 ) {
-    const {id}= await params;
+    const { id } = await params;
     console.log("Fetching order with ID:", id);
-  const order = await prisma.order.findUnique({ where: { id } });
-  if (!order) {
-    return NextResponse.json({ error: "Order not found" }, { status: 404 });
-  }
-  console.log("Fetched order:", order);
-  return NextResponse.json(order);
+    const order = await prisma.order.findUnique({ where: { id } });
+    if (!order) {
+        return NextResponse.json({ error: "Order not found" }, { status: 404 });
+    }
+    console.log("Fetched order:", order);
+    return NextResponse.json(order);
 }
 
 export async function PATCH(
-  req: NextRequest,
-  { params }: PageProps
+    req: NextRequest,
+    { params }: PageProps
 ) {
-    const {id}= await params;
+    const { id } = await params;
     // console.log("Fetching order with ID:", id);
-  const body = await req.json();
-  const order = await prisma.order.update({
-    where: { id: id },
-    data: body, // e.g. { status: "DELIVERED", deliveredAt: new Date().toISOString() }
-  });
-  return NextResponse.json(order);
+    const body = await req.json();
+    const order = await prisma.order.update({
+        where: { id: id },
+        data: body, // e.g. { status: "DELIVERED", deliveredAt: new Date().toISOString() }
+    });
+    return NextResponse.json(order);
 }
